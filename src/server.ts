58619ff95f614869f424f00f0334205968757e78
PedroReves <pedroreves11@gmail.com>
2024-07-09 19:54:02 -0300

    Chore: Register the Routes in Fastify

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import fastify from "fastify";
 import createTrip from "./routes/create-trip";
 import {
-  serializerCompiler,
-  validatorCompiler,
+	serializerCompiler,
+	validatorCompiler,
 } from "fastify-type-provider-zod";
 import cors from "@fastify/cors";
 import confirmTrip from "./routes/confirm-trip";
+import confirmParticipant from "./routes/confirm-participant";
+import createActivity from "./routes/create-activity";
+import getActivities from "./routes/get-activities";
+import createLink from "./routes/create-link";
+import getLinks from "./routes/get-links";
 
 const app = fastify();
 
@@ -13,11 +18,16 @@ app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
 app.register(cors, {
-  origin: "*",
+	origin: "*",
 });
 app.register(createTrip);
 app.register(confirmTrip);
+app.register(confirmParticipant);
+app.register(createActivity);
+app.register(getActivities);
+app.register(createLink);
+app.register(getLinks);
 
 app.listen({ port: 3333 }).then(() => {
-  console.log("Servidor Rodando na Porta 3333!");
+	console.log("Servidor Rodando na Porta 3333!");
 });
