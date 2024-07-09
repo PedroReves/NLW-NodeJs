9422d1e94765ed0983bd5f2da0d9c85fef0af885
PedroReves <pedroreves11@gmail.com>
2024-07-08 23:29:51 -0300

    First Part of the Project

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import fastify from "fastify";
+import createTrip from "./routes/create-trip";
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from "fastify-type-provider-zod";
+import cors from "@fastify/cors";
+import confirmTrip from "./routes/confirm-trip";
+
+const app = fastify();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(cors, {
+  origin: "*",
+});
+app.register(createTrip);
+app.register(confirmTrip);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("Servidor Rodando na Porta 3333!");
+});
